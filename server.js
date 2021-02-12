0563bbc3585bd014ab9e47bab2c71cc5f79087f6
pandorum2096 <toureabdouljunior@gmail.com>
2021-02-12 15:40:40 +0000

    return

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 let assignment = require('./routes/assignments');
-let connexion = require('./routes/connexions');
-
 
 let mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
@@ -11,9 +9,9 @@ mongoose.Promise = global.Promise;
 
 // remplacer toute cette chaine par l'URI de connexion à votre propre base dans le cloud s
 const uri = 'mongodb+srv://toureabdouljunior:TOUre2096@cluster0.y4qhl.mongodb.net/assignments?retryWrites=true&w=majority';
-const uri2 = 'mongodb+srv://toureabdouljunior:TOUre2096@cluster0.y4qhl.mongodb.net/connexions?retryWrites=true&w=majority';
+
 //localhost
-//const uri2 = 'umongodb://localhost:27017/assignments';
+const uri2 = 'umongodb://localhost:27017/assignments';
 
 const options = {
   useNewUrlParser: true,
@@ -30,21 +28,6 @@ mongoose.connect(uri, options)
     err => {
       console.log('Erreur de connexion: ', err);
     });
-
-
-    mongoose.connect(uri2, options)
-    .then(() => {
-      console.log("Connecté à la base MongoDB connexions dans le cloud !");
-      console.log("at URI = " + uri2);
-      console.log("vérifiez with http://localhost:8010/api/connexions que cela fonctionne")
-      },
-      err => {
-        console.log('Erreur de connexion: ', err);
-      });
-
-
-
-
 
 // Pour accepter les connexions cross-domain (CORS)
 app.use(function (req, res, next) {
@@ -74,21 +57,6 @@ app.route(prefix + '/assignments/:id')
 app.route(prefix + '/assignments')
   .post(assignment.postAssignment)
   .put(assignment.updateAssignment);
-////////////////////////////////////////
-
-app.route(prefix + '/connexions')
-  .get(connexion.getConnexions);
-
-app.route(prefix + '/connexions/:id')
-  .get(connexion.getConnexion)
-  .delete(connexion.deleteConnexion);
-
-
-app.route(prefix + '/connexions')
-  .post(connexion.postConnexion)
-  .put(connexion.updateConnexion);
-
-
 
 // On démarre le serveur
 app.listen(port, "0.0.0.0");
