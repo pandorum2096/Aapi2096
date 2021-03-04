80d3a0acc9eb66f55600d1d5da5d622050b463fc
pandorum2096 <toureabdouljunior@gmail.com>
2021-03-04 12:36:22 +0000

    retour

--- routes/assignments.js
+++ routes/assignments.js
@@ -1,7 +1,7 @@
 let Assignment = require('../model/assignment');
 
 // Récupérer tous les assignments (GET)
-/*function getAssignments(req, res){
+function getAssignments(req, res){
     Assignment.find((err, assignments) => {
         if(err){
             res.send(err)
@@ -9,27 +9,7 @@ let Assignment = require('../model/assignment');
 
         res.send(assignments);
     });
-}*/
-
-
-// AVEC PAGINATION
-function getAssignments(req, res) {
-    var aggregateQuery = Assignment.aggregate();
-    Assignment.aggregatePaginate(
-      aggregateQuery,
-      {
-        page: parseInt(req.query.page) || 1,
-        limit: parseInt(req.query.limit) || 10,
-      },
-      (err, assignments) => {
-        if (err) {
-          res.send(err);
-        }
-  
-        res.send(assignments);
-      }
-    );
-  }
+}
 
 // Récupérer un assignment par son id (GET)
 function getAssignment(req, res){
